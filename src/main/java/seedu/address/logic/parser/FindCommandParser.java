8b4d6049d6da8be07a6091c305710e60c0f8304b
eman-kom <emmanuelmok92@gmail.com>
2022-03-09 23:36:24 +0800

    Multiple Find

--- src/main/java/seedu/address/logic/parser/FindCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindCommandParser.java
@@ -1,12 +1,15 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.*;
 
-import java.util.Arrays;
+import java.lang.reflect.Array;
+import java.util.*;
 
 import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.NameContainsKeywordsPredicate;
+import seedu.address.model.tag.Tag;
 
 /**
  * Parses input arguments and creates a new FindCommand object
@@ -20,14 +23,53 @@ public class FindCommandParser implements Parser<FindCommand> {
      */
     public FindCommand parse(String args) throws ParseException {
         String trimmedArgs = args.trim();
+
         if (trimmedArgs.isEmpty()) {
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
         }
 
+        ArgumentMultimap argMultimap =
+                ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_ADDRESS, PREFIX_TAG);
+
+        HashMap<Prefix, String> searchStrings = new HashMap<>();
+
+        if (argMultimap.getValue(PREFIX_NAME).isPresent()) {
+            String name = ParserUtil.parseName(argMultimap.getValue(PREFIX_NAME).get()).toString();
+            searchStrings.put(PREFIX_NAME, name);
+        }
+
+        if (argMultimap.getValue(PREFIX_PHONE).isPresent()) {
+            String phone = ParserUtil.parsePhone(argMultimap.getValue(PREFIX_PHONE).get()).toString();
+            searchStrings.put(PREFIX_PHONE, phone);
+        }
+
+        if (argMultimap.getValue(PREFIX_EMAIL).isPresent()) {
+            String email = ParserUtil.parseEmail(argMultimap.getValue(PREFIX_EMAIL).get()).toString();
+            searchStrings.put(PREFIX_EMAIL, email);
+        }
+
+        if (argMultimap.getValue(PREFIX_ADDRESS).isPresent()) {
+            String address = ParserUtil.parseAddress(argMultimap.getValue(PREFIX_ADDRESS).get()).toString();
+            searchStrings.put(PREFIX_ADDRESS, address);
+        }
+
+        if (argMultimap.getValue(PREFIX_TAG).isPresent()) {
+            Set<Tag> tagSet = ParserUtil.parseTags(argMultimap.getAllValues(PREFIX_TAG));
+
+            StringBuilder tags = new StringBuilder();
+
+            for (Tag t : tagSet) {
+                tags.append(t.toString()).append(" ");
+            }
+            searchStrings.put(PREFIX_TAG, tags.toString().trim());
+        }
+
+
         String[] nameKeywords = trimmedArgs.split("\\s+");
 
-        return new FindCommand(new NameContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
+        // return new FindCommand(new NameContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
+        return new FindCommand(new NameContainsKeywordsPredicate(searchStrings));
     }
 
 }
--- src/main/java/seedu/address/model/person/NameContainsKeywordsPredicate.java
+++ src/main/java/seedu/address/model/person/NameContainsKeywordsPredicate.java
@@ -1,24 +1,86 @@
 package seedu.address.model.person;
 
-import java.util.List;
+import java.util.*;
 import java.util.function.Predicate;
 
 import seedu.address.commons.util.StringUtil;
+import seedu.address.logic.parser.Prefix;
+import seedu.address.model.tag.Tag;
+
+import static seedu.address.logic.parser.CliSyntax.*;
 
 /**
  * Tests that a {@code Person}'s {@code Name} matches any of the keywords given.
  */
 public class NameContainsKeywordsPredicate implements Predicate<Person> {
-    private final List<String> keywords;
+    private final HashMap<Prefix, String> keywords;
 
-    public NameContainsKeywordsPredicate(List<String> keywords) {
+    public NameContainsKeywordsPredicate(HashMap<Prefix, String> keywords) {
         this.keywords = keywords;
     }
 
     @Override
     public boolean test(Person person) {
-        return keywords.stream()
-                .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(person.getName().fullName, keyword));
+
+        if (this.keywords.containsKey(PREFIX_NAME)) {
+            String[] nameFragments = this.keywords.get(PREFIX_NAME).split(" ");
+
+            for (String frag : nameFragments) {
+                boolean lmao = StringUtil.containsWordIgnoreCase(person.getName().fullName, frag);
+
+                if (!lmao) {
+                    return false;
+                }
+            }
+        }
+
+        if (this.keywords.containsKey(PREFIX_PHONE)) {
+            String phone = this.keywords.get(PREFIX_PHONE);
+
+            if (!person.getPhone().toString().equals(phone)) {
+                return false;
+            }
+        }
+
+        if (this.keywords.containsKey(PREFIX_EMAIL)) {
+            String email = this.keywords.get(PREFIX_EMAIL);
+
+            if (!person.getEmail().toString().contains(email)) {
+                return false;
+            }
+        }
+
+        if (this.keywords.containsKey(PREFIX_ADDRESS)) {
+            String[] addrFrags = this.keywords.get(PREFIX_ADDRESS).split(" ");
+
+            for (String frag : addrFrags) {
+                boolean lmao = StringUtil.containsWordIgnoreCase(person.getAddress().toString(), frag);
+
+                if (!lmao) {
+                    return false;
+                }
+            }
+        }
+
+        if (this.keywords.containsKey(PREFIX_TAG)) {
+
+            String[] tagList = this.keywords.get(PREFIX_TAG).split(" ");
+            Set<Tag> search = new HashSet<>();
+
+            for (String frag : tagList) {
+                String tt = frag.replaceAll("[^a-zA-Z0-9]", "");
+                search.add(new Tag(tt));
+            }
+
+            Set<Tag> tagSet = person.getTags();
+
+            if (!tagSet.containsAll(search)) {
+                return false;
+            }
+
+        }
+
+        return true;
     }
 
     @Override
