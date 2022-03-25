c59e88212ab0099597050c6659d80e2c39b476c7
eman-kom <emmanuelmok92@gmail.com>
2022-03-25 14:15:32 +0800

    Init Import Command

--- src/main/java/seedu/address/logic/commands/ImportCommand.java
+++ src/main/java/seedu/address/logic/commands/ImportCommand.java
@@ -1,13 +1,18 @@
 package seedu.address.logic.commands;
 
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 
 public class ImportCommand extends Command {
 
     public static final String COMMAND_WORD = "import";
 
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Imports job applicant details from CSV file";
+
+    public static final String MESSAGE_NOT_IMPLEMENTED_YET = "Import Command is not implement yet";
+
     @Override
-    public CommandResult execute(Model model) {
-        return new CommandResult("Hello from import");
+    public CommandResult execute(Model model) throws CommandException {
+        throw new CommandException(MESSAGE_NOT_IMPLEMENTED_YET);
     }
 }
